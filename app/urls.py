4530d0456569d30467e61655ed9df748f1e726d0
joaoguilherme222 <joaolinomartins@gmail.com>
2026-03-05 04:05:47 -0300

    feat: criação da API para Editora

--- app/urls.py
+++ app/urls.py
@@ -7,12 +7,13 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, CategoriaViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
+router.register(r'editoras', EditoraViewSet, basename='editoras')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
